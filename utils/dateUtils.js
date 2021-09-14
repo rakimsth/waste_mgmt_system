7a6e3dfc605cad7249c4e185e41ae3e46c65861c
rakimsth <raktim@rumsan.com>
2021-09-14 21:54:37 +0545

    user task list

--- utils/dateUtils.js
+++ utils/dateUtils.js
@@ -2,4 +2,5 @@
 - date formatter based on date and format Eg: dateFormatter(date,format)
 - datechecker for 18 yrs above age
 - convert date to age
+- Current Epoch unix timestamp generator (should generate 1631635697 number)
 */
