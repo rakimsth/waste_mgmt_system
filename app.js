0c071c6eb293088012f2c5a6995058b9b0552fcb
rakimsth <raktim@rumsan.com>
2021-10-04 20:59:42 +0545

    cleanup error handling in app.js

--- app.js
+++ app.js
@@ -22,37 +22,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDoc, { explorer: true }));
 app.use('/', routeManager);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      success: false,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500);
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
   res.json({
-    message: err.message
+    status: error.status || 500,
+    message: process.env.NODE_ENV === 'development' ? error.message : '',
+    stack: process.env.NODE_ENV === 'development' ? error.stack : ''
   });
 });
-
 module.exports = app;
